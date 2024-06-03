10c776134b19f17bc0238856a71f6a26968b7faa
Eduardo Função <eduardofuncao@hotmail.com>
2024-06-03 13:06:03 -0300

    setup page details

--- app.py
+++ app.py
@@ -2,23 +2,26 @@ import streamlit as st
 import qrcode
 import qrcode.image.svg
 
+st.set_page_config(
+    page_title="Gerador de QRcode",
+    page_icon="📷",
+)
+
 st.write("# Geração de QRcode")
-link = st.text_input("### Digite o **link do QRcode** abaixo e aperte enter:", "seu link aqui")
-# st.image('testefundobranco.svg', caption='qrcode')
-# https://front.petz.com.br/checkin-customer?subsidiary=04%200030
+link = st.text_input("### Digite o **link do QRcode** abaixo e aperte enter:", "Seu link aqui")
 
 def generateQRcode(link):
     img = qrcode.make(link, image_factory= qrcode.image.svg.SvgPathFillImage)
-    img.save("testefundobranco.svg")
+    img.save("qrcode-gerado")
 
 
 if st.button('Gerar QRcode'):
     generateQRcode(link)
-    st.image('testefundobranco.svg', caption=f'qrcode para {link}')
-    with open("testefundobranco.svg", "rb") as file:
+    st.image('qrcode-gerado', caption=f'qrcode para {link}')
+    with open("qrcode-gerado", "rb") as file:
         btn = st.download_button(
                 label="Baixar QRcode como SVG",
                 data=file,
-                file_name="qrcode-{link}.svg",
+                file_name=f"qrcode-{link}.svg",
                 mime="image/svg"
             )
